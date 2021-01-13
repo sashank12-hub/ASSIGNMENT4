0df2dd6a5f1a2ca30f49ba6ddba029bad38ce1cf
sashank12-hub <58355991+sashank12-hub@users.noreply.github.com>
2021-01-13 12:48:22 +0530

    1st

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./about.css";
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<div className="about">
+
+        <h4></h4>
+        </div>  );
+    }
+}
+ 
+export default About ;
